d4f5699985b8eea864fa0895673e1fe074dc9f74
Felix Seiboldt <52862870+devilAPI@users.noreply.github.com>
2021-07-08 15:27:23 +0200

    Change clear to Clear-Host

--- SpotifyTools.ps1
+++ SpotifyTools.ps1
@@ -1,4 +1,4 @@
-﻿clear
+﻿Clear-Host
 Write-Host "Welcome to SpotifyTools!"
 Write-Host "Please select wich Tool you want to use."
 Write-Host ""
@@ -7,7 +7,7 @@ Write-Host "2 - BlockTheSpot Installer"
 Write-Host "3 - Auto-Installer (for faster Installs)"
 Write-Host ""
 $toolid = Read-Host 'Please select'
-clear
+Clear-Host
 Write-Host "You chose $toolid!"
 if($toolid -eq 1) {
     Write-Host "SpicetifyHelper is not available yet."
@@ -16,7 +16,7 @@ elseif($toolid -eq 2) {
     Write-Host "Downloading BTS Installer..."
     Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/mrpond/BlockTheSpot/master/BlockTheSpot.bat" -OutFile "BlockTheSpotInstaller.bat"
     Write-Host "Done, running BTS Installer..."
-    clear
+    Clear-Host
     Start-Process "cmd.exe"  "/c BlockTheSpotInstaller.bat"
 }
 elseif($toolid -eq 3) {
@@ -25,7 +25,7 @@ elseif($toolid -eq 3) {
     Write-Host "1 - DribblishDynamic v2"
     Write-Host ""
     $autoinstallerid = Read-Host 'Please select'
-    clear
+    Clear-Host
     if($autoinstallerid -eq 1) {
         Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/JulienMaille/dribbblish-dynamic-theme/master/install.ps1" | Invoke-Expression
     }
