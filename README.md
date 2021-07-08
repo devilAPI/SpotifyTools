1dae0e636c5d12312495c4f7b645abf791b58a1e
Felix Seiboldt <52862870+devilAPI@users.noreply.github.com>
2021-07-08 15:18:42 +0200

    Add main files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # SpotifyTools
 A small PowerShell Script providing tweaks to the Spotify Desktop Application.
+
+## Features
+
+* Spicetify Installer
+* BlockTheSpot Installer
+* Auto Installers
+* * DribblishDynamic v2
---
+++ SpotifyTools.ps1
@@ -0,0 +1,35 @@
+﻿clear
+Write-Host "Welcome to SpotifyTools!"
+Write-Host "Please select wich Tool you want to use."
+Write-Host ""
+Write-Host "1 - SpicetifyHelper"
+Write-Host "2 - BlockTheSpot Installer"
+Write-Host "3 - Auto-Installer (for faster Installs)"
+Write-Host ""
+$toolid = Read-Host 'Please select'
+clear
+Write-Host "You chose $toolid!"
+if($toolid -eq 1) {
+    Write-Host "SpicetifyHelper is not available yet."
+}
+elseif($toolid -eq 2) {
+    Write-Host "Downloading BTS Installer..."
+    Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/mrpond/BlockTheSpot/master/BlockTheSpot.bat" -OutFile "BlockTheSpotInstaller.bat"
+    Write-Host "Done, running BTS Installer..."
+    clear
+    Start-Process "cmd.exe"  "/c BlockTheSpotInstaller.bat"
+}
+elseif($toolid -eq 3) {
+    Write-Host "Please select wich Installer you want to run"
+    Write-Host ""
+    Write-Host "1 - DribblishDynamic v2"
+    Write-Host ""
+    $autoinstallerid = Read-Host 'Please select'
+    clear
+    if($autoinstallerid -eq 1) {
+        Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/JulienMaille/dribbblish-dynamic-theme/master/install.ps1" | Invoke-Expression
+    }
+    else { Write-Host "This isntaller dosent exist."}
+}
+else { Write-Host "This tool dosent exist."}
+exit
